8487b051121ee55ed02273c34240e310d1f75f64
web12-app <web12-app@users.noreply.github.com>
2026-09-10 09:32:01 +0000

    fix: compilerOptions under kotlin{} extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
     buildFeatures {
         compose = true
     }
@@ -73,4 +70,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
